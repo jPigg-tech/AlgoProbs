b9a2b0cfd1657547618adbeaee8706428d6c2ff3
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-04-29 21:54:47 -0500

    Created a method to check if a pin is valid bu passing in a string. I used the TryParse method to check if the pin is a valid int and then checked the length of the pin if it matches 4 or 6 digits as expressed in the instructions

--- AlgoProblems/WhiteBoardChallenges.cs
+++ AlgoProblems/WhiteBoardChallenges.cs
@@ -6,5 +6,24 @@ namespace AlgoProblems
 {
     class WhiteBoardChallenges
     {
+
+        public static bool IsPinValid(string pin)
+        {
+            if (Int32.TryParse(pin, out int numValue) == true)
+            {
+                if (pin.Length == 4 || pin.Length == 6)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
