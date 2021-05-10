4b61c25c72ddf153b9233a1094b7cc72a1ad9339
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-05-10 11:17:45 -0500

    Created a RunningSumArray class and implemented a method RunningSum that takes in an array of ints and combines the running sum of those inta nd returns a new array of the sums

---
+++ AlgoProblems/RunnningSumArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoProblems
+{
+    class RunnningSumArray
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            int[] numsReturned = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                if (i == 0)
+                {
+                    numsReturned[0] = nums[0];
+                }
+                if (i > 0)
+                {
+                    numsReturned[i] = numsReturned[i - 1] + nums[i];
+                }
+            }
+            return numsReturned;
+        }
+
+        // Input: nums = [1,2,3,4]
+        // Output: [1,3,6,10]
+        // Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+    }
+}
