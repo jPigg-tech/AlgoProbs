8fed3474c6127b57044adb4440b7fae37074d5ee
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-05-19 17:07:51 -0500

    Added an Algo for Alphabet Soup as well

--- AlgoProblems/AlphabetSoup.cs
+++ AlgoProblems/AlphabetSoup.cs
@@ -9,7 +9,42 @@ namespace AlgoProblems
     {
         public string GetAlphabetSoup(string str)
         {
-            
+            //string newWord;
+            //var wordArray = new char[word.Length];
+            //var wordArray2 = new List<char>();
+            //for (int i = 0; i < word.Length - 1; i++)
+            //{
+            //    wordArray2.Add(word[i]);
+            //}
+
+            //wordArray2.Sort();
+            //// string newWord = wordArray2.ToString();
+            //foreach (var character in wordArray2)
+            //{
+
+            //}
+
+            //return newWord;
+
+
+            // return String.Concat(str.OrderBy(s => s));
+
+            char[] chArray = str.ToArray();
+            int length = chArray.Length;
+            char ch = ' ';
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (chArray[i] > chArray[j])
+                    {
+                        ch = chArray[i];
+                        chArray[i] = chArray[j];
+                        chArray[j] = ch;
+                    }
+                }
+            }
+            return new string(chArray);
 
 
         }
